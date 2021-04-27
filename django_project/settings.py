952f924195f31b57a894b9595fb8713d8ff7a6ac
prayaagmahajan <64199797+prayaagmahajan@users.noreply.github.com>
2021-04-27 21:38:17 +0530

    Update settings.py

--- django_project/settings.py
+++ django_project/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '&2#(+=*)wpbtb8-4b2a9b&-t5)+ug1ooobo1t==u2^)q)8089+'
+SECRET_KEY = FALSE
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -136,5 +136,5 @@ EMAIL_BACKEND ='django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = 'prayaagmahajan@gmail.com'
-EMAIL_HOST_PASSWORD = 'cqlwwijgtlmkmedd'
+EMAIL_HOST_USER = 'YOUR_EMAIL_HERE'
+EMAIL_HOST_PASSWORD = 'PASSWORD'
